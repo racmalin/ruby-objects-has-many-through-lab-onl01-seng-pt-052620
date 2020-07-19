b16ffd5f02f2ec64ece793c4e35fada094f7ff2b
Kevin C Malin <racmalin@yahoo.com>
2020-07-19 01:46:52 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,28 +1,27 @@
-require 'pry'
-
 class Doctor
   
   attr_accessor :name
+
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self 
-    @appointments = []
+    @@all << self
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.doctor = self
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
   end
 
   def appointments
-    @appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
   end
 
   def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
+    appointments.map(&:patient)
   end
 end
